103b031be590fbd0fa802502bd8192ef5f0bc53f
egioe2 <158243906+egioe2@users.noreply.github.com>
2024-11-09 22:22:21 -0600

    Moving files
    Moving files because of name change

---
+++ RunNGun/Levels/Tutorial/NextLevelPortal1.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class NextLevelPortal1 : Area2D
+{
+   
+    [Export] public string NextLevelScenePath = "res://Levels/Tutorial/Main.tscn";
+
+    public override void _Ready()
+    {
+       
+        BodyEntered += OnBodyEntered;
+    }
+
+    private void OnBodyEntered(Node2D body)
+    {
+      
+        if (body is Character)  
+        {
+            // Load the next level scene.
+            GetTree().ChangeSceneToFile("res://Levels/Tutorial/Main.tscn");
+        }
+    }
+}
