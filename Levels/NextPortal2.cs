54c8d1bbe3153c2c67ab52a18669ccb10c42335f
RPaul0012 <148822578+RPaul0012@users.noreply.github.com>
2024-11-19 19:31:39 -0600

    Tutorial Level
    Adds fully functional tutorial

---
+++ Levels/NextPortal2.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class NextPortal2 : Area2D
+{
+        public override void _Ready()
+    {
+       
+        BodyEntered += OnBodyEntered;
+    }
+
+    private void OnBodyEntered(Node2D body)
+    {
+      
+        if (body is Character)  
+        {
+            // Load the next level scene.
+            GetTree().ChangeSceneToFile("res://Levels/Tutorial3.tscn");
+        }
+    }
+}
