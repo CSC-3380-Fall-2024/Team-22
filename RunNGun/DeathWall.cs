403750bb9952d48611b88d48dfc626a2edf7f993
RPaul0012 <148822578+RPaul0012@users.noreply.github.com>
2024-12-02 13:53:05 -0600

    DeathWall dependencies

---
+++ RunNGun/DeathWall.cs
@@ -0,0 +1,13 @@
+using Godot;
+
+public partial class DeathWall : Area2D
+{
+    [Signal]
+	public delegate void damagingEventHandler();
+	    public override void _Ready()
+    {
+       
+    }
+
+
+}
