d62ae37cf36a41b1b16fa58c1a8e7f0ea5879996
RPaul0012 <148822578+RPaul0012@users.noreply.github.com>
2024-12-07 22:13:31 -0600

    Death Barrier
    Adds Scene for Death Barrier that kills player.

---
+++ RunNGun/Characters/EnemyTest.cs
@@ -0,0 +1,66 @@
+using Godot;
+using System;
+
+public partial class EnemyTest : CharacterBody2D
+{
+	// Marks node as being damaging to player
+	[Signal]
+	public delegate void damagingEventHandler();
+	private const float SPEED = 100f;
+	private int health = 20;
+	private int moveDir = -1;
+	private KinematicCollision2D hit;
+	private Vector2 velocity;
+
+
+	// Used for detecting collisions with environment
+	private RayCast2D[] downCasts = new RayCast2D[2];
+	private RayCast2D frontCast;
+
+    public override void _Ready()
+    {
+		downCasts[0] = (RayCast2D)GetNode("DownLeft");
+		downCasts[1] = (RayCast2D)GetNode("DownRight");
+		frontCast = (RayCast2D)GetNode("Front");
+  	}
+
+    public override void _Process(double delta)
+    {
+		
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {	
+
+		hit = GetLastSlideCollision();
+		if (hit != null)
+		{
+			if (hit.GetCollider().HasSignal("projectile"))
+			{
+				health -= 20;
+			}
+
+			if (hit.GetCollider().HasSignal("leafBullet"))
+			{
+				health -= 10;
+			}
+		}
+    }
+
+	public Godot.Collections.Dictionary<string, Variant> Save()
+	{
+		return new Godot.Collections.Dictionary<string, Variant>()
+		{
+			{ "Filename", SceneFilePath },
+			{ "Parent", GetParent().GetPath() },
+			{ "PosX", Position.X }, 
+			{ "PosY", Position.Y },
+			{ "MoveDir", moveDir},
+			{ "Health", health},
+			{ "FrontCast", frontCast},
+			{ "DownCasts", downCasts},
+		};
+	}
+
+	
+}
