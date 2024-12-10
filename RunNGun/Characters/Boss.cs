b86f9cda5eddab6d0faece445c48166c8bab055a
RPaul0012 <148822578+RPaul0012@users.noreply.github.com>
2024-12-10 17:13:55 -0600

    Game Update

---
+++ RunNGun/Characters/Boss.cs
@@ -0,0 +1,75 @@
+using Godot;
+using System;
+
+public partial class Boss : CharacterBody2D
+{
+	// Marks node as being damaging to player
+	[Signal]
+	public delegate void damagingEventHandler();
+	private const float SPEED = 0f;
+	private int health = 100;
+	private int moveDir = -1;
+	private KinematicCollision2D hit;
+	private Vector2 velocity;
+
+
+	// Used for spawning enemies
+	private RayCast2D frontCast;
+
+    public override void _Ready()
+    {
+		frontCast = (RayCast2D)GetNode("Front");
+  	}
+
+    public override void _Process(double delta)
+    {
+		if (isDead()) QueueFree();
+
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {	
+		velocity = Velocity;
+
+		velocity.X = SPEED*moveDir;
+		Velocity = velocity;
+		MoveAndSlide();
+
+		hit = GetLastSlideCollision();
+		if (hit != null)
+		{
+			if (hit.GetCollider().HasSignal("projectile"))
+			{
+				health -= 20;
+			}
+
+			if (hit.GetCollider().HasSignal("leafBullet"))
+			{
+				health -= 10;
+			}
+		}
+
+		
+    }
+
+	public Godot.Collections.Dictionary<string, Variant> Save()
+	{
+		return new Godot.Collections.Dictionary<string, Variant>()
+		{
+			{ "Filename", SceneFilePath },
+			{ "Parent", GetParent().GetPath() },
+			{ "PosX", Position.X }, 
+			{ "PosY", Position.Y },
+			{ "MoveDir", moveDir},
+			{ "Health", health},
+			{ "FrontCast", frontCast},
+		};
+	}
+
+	private bool isDead()
+	{
+		if (health <= 0) return true;
+
+		else return false;
+	}
+}
--- RunNGun/NPC's/NPC4.cs
+++ RunNGun/NPC's/NPC4.cs
@@ -11,14 +11,14 @@ public partial class NPC4 : CharacterBody2D
 	public override void _Ready()
 	{
 		
-		InterfaceSelectionObject obj = new InterfaceSelectionObject(1, "Exploring.");
-		InterfaceSelectionObject obj2 = new InterfaceSelectionObject(2, "Treasure Hunting");
-		InterfaceSelectionObject obj3 = new InterfaceSelectionObject(-1, "Thanks");
+		InterfaceSelectionObject obj = new InterfaceSelectionObject(1, "Advice?");
+		InterfaceSelectionObject obj2 = new InterfaceSelectionObject(2, "Trident?");
+		InterfaceSelectionObject obj3 = new InterfaceSelectionObject(-1, "Cool");
 		npc4Dialogue = new List<NpcDialogue>
 		{
-			new NpcDialogue(new List<InterfaceSelectionObject>(){obj,obj2},"Hello young one. Tell me, what are you doing in these woods?", 0),
-			new NpcDialogue(new List<InterfaceSelectionObject>(){obj3},"You must be cautious of the monsters in this place.", 1),
-			new NpcDialogue(new List<InterfaceSelectionObject>(){obj3},"You may not like what you find...", 2)
+			new NpcDialogue(new List<InterfaceSelectionObject>(){obj,obj2},"HEY THERE! There aren't many fish in this area, but I'll CATCH THEM ANYWAY! You want the treasure huh? Well you might want some advice.", 0),
+			new NpcDialogue(new List<InterfaceSelectionObject>(){obj3},"Don't get in the water. It's pretty acidy. You see those floating islands? If you jump off the farthest one, you'll find a magic ? block.", 1),
+			new NpcDialogue(new List<InterfaceSelectionObject>(){obj3},"I like fishing with this thing, it kinda burns in the water though.", 2)
 
 		};
 		npc4Name = "The Fisherman";
